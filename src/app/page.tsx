b1780e68573705ce73f02e8e22bfadab5fedc20e
orelsprojects <orelsmail@gmail.com>
2024-08-09 20:38:36 +0300

    add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Image from "next/image";
+import { Button } from "../components/ui/button";
 
 export default function Home() {
   const router = useRouter();
-  useEffect(() => {
-    router.push("/login");
-  }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-10">
+      <div className="w-full flex flex-row justify-center items-center">
+        <Image
+          src="/favicon.png"
+          alt="MeetMeter Logo"
+          width={100}
+          height={100}
+        />
+        <h1 className="text-3xl font-bold text-foreground">MeetMeter</h1>
+      </div>
+      <h1>Track your meetings and get insights so you can improve!</h1>
+      <Button onClick={() => router.push("/login")}>Get Started!</Button>
+    </main>
   );
 }
